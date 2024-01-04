203bf8ff54bc37ac176f178b678ddc5c78965014
Chris <chrisbotros10@gmail.com>
2024-01-04 14:09:40 -0500

    fix: `retrieve` method in `URLMappingViewSet` no longer errors is the redis cache is not present

--- server/config/mapperApp/api/views.py
+++ server/config/mapperApp/api/views.py
@@ -1,7 +1,6 @@
 from django.core.cache import cache
 from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 from ..models import URLMapping
@@ -12,15 +11,20 @@ class URLMappingViewSet(ModelViewSet):
   serializer_class = URLMappingSerializer
 
   def retrieve(self, request: Request, pk: str | None = None):
+    has_cache = True
     if pk != None:
-      cached_data = cache.get(pk)
-      if cached_data != None:
-        return Response(cached_data)
+      try:
+        cached_data = cache.get(pk)
+        if cached_data != None:
+          return Response(cached_data)
+      except Exception as e:
+        has_cache = False
+        print(e)
 
     mapping = get_object_or_404(self.queryset, pk=pk)
     serializer = self.serializer_class(mapping)
 
-    if pk != None:
+    if pk != None and has_cache:
       cache.set(pk, serializer.data)
   
     return Response(serializer.data)
