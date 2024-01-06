30e7c438f7d1e2e2f6f5668d88faa2448a8f599e
Chris <chrisbotros10@gmail.com>
2024-01-06 18:32:26 -0500

    refactor: remove `All Mappings` section from root page

--- client/app/src/App.tsx
+++ client/app/src/App.tsx
@@ -6,7 +6,6 @@ import urlMappingsApi, { UrlMapping } from './api/urlMappings';
 function App() {
   const [url, setUrl] = React.useState('')
   const [urlKey, setUrlKey] = React.useState('')
-  const [mappedUrls, setMappedUrls] = React.useState<UrlMapping[]>([])
 
   const handleUrlInputChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     setUrl(event.target.value)
@@ -24,20 +23,10 @@ function App() {
 
     urlMappingsApi.post(data).then(res => {
       console.log(res)
-      setMappedUrls([...mappedUrls, data])
     }).catch(err => {
       console.log(err)
     });
   }
-
-  React.useEffect(() => {
-    urlMappingsApi.getAll().then(res => {
-      setMappedUrls([...res.data]);
-    }).catch(err => {
-      console.log(err)
-    });
-    
-  }, [])
 
   return (
     <div className="App">
@@ -46,10 +35,6 @@ function App() {
         urlTextFieldProps={{onChange: handleUrlInputChange, value: url}} 
         urlKeyTextFieldProps={{onChange: handleUrlKeyInputChange, value: urlKey}}  
       />
-      <h2>All Mappings:</h2>
-      <div>
-        {mappedUrls.map(mappedUrl => <div key={mappedUrl.urlKey}><a href={mappedUrl.fullURL}>{mappedUrl.urlKey}</a></div>)}
-      </div>
     </div>
   );
 }
