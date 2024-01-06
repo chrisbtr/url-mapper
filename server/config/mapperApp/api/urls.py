df1c686c07e0c54cb49dbbb550725766ba111ecd
Chris <chrisbotros10@gmail.com>
2024-01-06 17:52:10 -0500

    chore: remove commented out code

--- server/config/mapperApp/api/urls.py
+++ server/config/mapperApp/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import URLMappingViewSet #, RedirectView
+from .views import URLMappingViewSet 
 
 router = DefaultRouter()
 router.register(r'api', URLMappingViewSet)
-# router.register(r'test', RedirectView)
