dc4d15d1ab36f95c8f9d67e36ff14a5645c249a3
Chris <chrisbotros10@gmail.com>
2024-01-07 14:41:49 -0500

    update `getAll` method in the `urlMappingsApi` object to account for pagination

--- client/app/src/api/urlMappings.ts
+++ client/app/src/api/urlMappings.ts
@@ -7,10 +7,19 @@ export type UrlMapping = {
 
 export type UrlMappingGetAllParams = { 
   search?: string,
+  page?: number,
+  page_size?: number,
+};
+
+export type UrlMappingGetAllResponse = {
+  count: number,
+  next: null | string,
+  previous: null | string,
+  results: UrlMapping[]
 };
 
 const urlMappingsApi = {
-  getAll: (params?: UrlMappingGetAllParams) => entryPoint.get<UrlMapping[]>("", { params: params }),
+  getAll: (params?: UrlMappingGetAllParams) => entryPoint.get<UrlMappingGetAllResponse>("", { params: params }),
   get: (key: string) => entryPoint.get<UrlMapping>(key),
   post: (data: UrlMapping) => entryPoint.post("", data),
 };
