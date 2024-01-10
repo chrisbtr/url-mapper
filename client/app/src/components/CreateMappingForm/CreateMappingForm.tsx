09493915e95f4f9a38f1984f871bf98dd60a93b9
Chris <chrisbotros10@gmail.com>
2024-01-10 15:00:56 -0500

    feat: add a `Your URL Mapping` to the CreateMapping form

--- client/app/src/components/CreateMappingForm/CreateMappingForm.tsx
+++ client/app/src/components/CreateMappingForm/CreateMappingForm.tsx
@@ -1,56 +1,91 @@
-import React from 'react'
-import Button from '@mui/material/Button';
-import Paper, { PaperProps } from '@mui/material/Paper';
-import TextField, { TextFieldProps } from '@mui/material/TextField';
+import React from "react";
+import Button from "@mui/material/Button";
+import Paper, { PaperProps } from "@mui/material/Paper";
+import TextField, { TextFieldProps } from "@mui/material/TextField";
+import Divider from "@mui/material/Divider";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 export interface CreateMappingFormProps {
-  urlTextFieldProps?:  TextFieldProps,
-  urlKeyTextFieldProps?: TextFieldProps,
-  paperFormProps?: PaperProps<'form'>,
+  urlTextFieldProps?: TextFieldProps;
+  urlKeyTextFieldProps?: TextFieldProps;
+  paperFormProps?: PaperProps<"form">;
 }
 
-const CreateMappingForm: React.FC<CreateMappingFormProps> = ({urlTextFieldProps, urlKeyTextFieldProps, paperFormProps}) => {
+const CreateMappingForm: React.FC<CreateMappingFormProps> = ({
+  urlTextFieldProps,
+  urlKeyTextFieldProps,
+  paperFormProps,
+}) => {
   return (
-    <Paper 
-      component="form" 
-      autoComplete='off'
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-        my: 1
-      }}
+    <Paper
+      component="form"
+      autoComplete="off"
+      sx={{ p: 2, my: 1 }}
       {...paperFormProps}
     >
-      <div>
-        <TextField 
+      <Box justifyContent="center">
+        <TextField
           required
+          sx={{ width: "25%", mb: 2 }}
           id="url-key-input"
-          size='small'
-          placeholder='example'
-          label='Key'
+          size="small"
+          placeholder="example"
+          label="Key"
           InputLabelProps={{
             shrink: true,
           }}
-
-          {...urlKeyTextFieldProps} 
+          {...urlKeyTextFieldProps}
         />
-      </div>
-      <div>
-        <TextField 
+      </Box>
+      <Box justifyContent="center">
+        <TextField
           required
+          sx={{ width: "25%", mb: 2 }}
           id="full-url-input"
-          size='small'
-          placeholder='https://example.com'
-          type='url' 
-          label='Website URL' 
+          size="small"
+          placeholder="https://example.com"
+          type="url"
+          label="Website URL"
           InputLabelProps={{
             shrink: true,
-          }} 
-          {...urlTextFieldProps} 
+          }}
+          {...urlTextFieldProps}
         />
-      </div>
-      <Button size='small' type='submit'>Create Mapping</Button>
+      </Box>
+      <Button size="small" type="submit" sx={{ mb: 1 }}>
+        Create Mapping
+      </Button>
+      <Divider sx={{ mb: 1 }} />
+      <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
+        Your URL Mapping:
+      </Typography>
+      <Typography
+        variant="subtitle1"
+        color={(theme) => theme.palette.text.secondary}
+        sx={{ display: "flex", justifyContent: "center" }}
+      >
+        <Box
+          color={
+            urlKeyTextFieldProps?.error
+              ? (theme) => theme.palette.error.main
+              : ""
+          }
+        >
+          {window.location.origin}/m/{String(urlKeyTextFieldProps?.value)}
+        </Box>
+        <ArrowForwardIcon />
+        <Box
+          color={
+            urlTextFieldProps?.error ? (theme) => theme.palette.error.main : ""
+          }
+        >
+          {String(urlTextFieldProps?.value)}
+        </Box>
+      </Typography>
     </Paper>
-  )
-}
+  );
+};
 
-export default CreateMappingForm
+export default CreateMappingForm;
