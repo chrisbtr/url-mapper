e2dbf6c949595fe68109346372eb8bc12cf474ce
Chris <chrisbotros10@gmail.com>
2024-01-27 13:53:58 -0500

    fix: respond with status 400 and an error message when a `ValidationError` occurs when registering an account and Login

--- server/config/mapperApp/validations.py
+++ server/config/mapperApp/validations.py
@@ -1,26 +1,28 @@
 from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+
 UserModel = get_user_model()
 
 def custom_validation(data):
   username = data['username'].strip()
   password = data['password'].strip()
   if not password or len(password) < 8:
-    raise ValidationError('choose another password, min 8 characters')
-  ##
+    raise ValidationError(_('choose another password, min 8 characters'), code='password')
+
   if not username:
-    raise ValidationError('choose another username')
+    raise ValidationError(_('choose another username'), code='username')
   return data
 
 
 def validate_username(data):
   username = data['username'].strip()
   if not username:
-    raise ValidationError('choose another username')
+    raise ValidationError(_('choose another username'), code='username')
   return True
 
 def validate_password(data):
   password = data['password'].strip()
   if not password:
-    raise ValidationError('a password is needed')
+    raise ValidationError(_('a password is needed'), code='password')
   return True
--- server/config/mapperApp/views.py
+++ server/config/mapperApp/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model, login, logout
 from django.core.cache import cache
+from django.core.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
 from rest_framework import permissions, status, filters
 from rest_framework.authentication import SessionAuthentication
@@ -16,13 +17,16 @@ class UserRegister(APIView):
   permission_classes = (permissions.AllowAny,)
 
   def post(self, request):
-    data = custom_validation(request.data)
-    serializer = UserRegisterSerializer(data=data)
-    if serializer.is_valid(raise_exception=True):
-      user = serializer.create(data)
-      if user:
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-      return Response(status=status.HTTP_400_BAD_REQUEST)
+    try:
+      data = custom_validation(request.data)
+      serializer = UserRegisterSerializer(data=data)
+      if serializer.is_valid(raise_exception=True):
+        user = serializer.create(data)
+        if user:
+          return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    except ValidationError as e:
+      return Response({e.code: [e.message]}, status=status.HTTP_400_BAD_REQUEST)
 
     
 class UserLogin(APIView):
@@ -30,15 +34,18 @@ class UserLogin(APIView):
   authentication_classes = (SessionAuthentication,)
 
   def post(self, request):
-    data = request.data
-    assert validate_password(data)
-    assert validate_username(data)
+    try: 
+      data = request.data
+      validate_password(data)
+      validate_username(data)
 
-    serializer = UserLoginSerializer(data=data)
-    if serializer.is_valid(raise_exception=True):
-      user = serializer.check_user(data)
-      login(request, user)
-      return Response(serializer.data, status=status.HTTP_200_OK)
+      serializer = UserLoginSerializer(data=data)
+      if serializer.is_valid(raise_exception=True):
+        user = serializer.check_user(data)
+        login(request, user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except ValidationError as e:
+      return Response({e.code: [e.message]}, status=status.HTTP_400_BAD_REQUEST)
 
 class UserLogout(APIView):
   def post(self, request):
