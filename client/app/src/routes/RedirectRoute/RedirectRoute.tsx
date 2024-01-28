9361b311a27898ae0b03b157bcbe15dc422e98da
Chris <chrisbotros10@gmail.com>
2024-01-28 14:12:37 -0500

    docs: add comments to the `RedirectRoute` component

--- client/app/src/routes/RedirectRoute/RedirectRoute.tsx
+++ client/app/src/routes/RedirectRoute/RedirectRoute.tsx
@@ -1,32 +1,34 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
-import urlMappingsApi from '../../api/urlMappings';
+import React from "react";
+import { useParams } from "react-router-dom";
+import urlMappingsApi from "../../api/urlMappings";
 
 type RedirectRouteParams = {
   urlKey: string;
-}
+};
 
 const RedirectRoute: React.FC = () => {
   const { urlKey } = useParams<RedirectRouteParams>();
 
+  // On component mount fetch the full URL using `urlKey` route parameter and redirect
   React.useEffect(() => {
     if (urlKey === undefined) {
       return;
     }
 
-    urlMappingsApi.get(urlKey).then(res => {
-      console.log(res)
-      if (res.status === 200 && res.data.fullURL !== urlKey) {
-        window.location.replace(res.data.fullURL)
-      }
-    }).catch(err => {
-      console.log(err)
-    });
-  }, [])
+    urlMappingsApi
+      .get(urlKey)
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200 && res.data.fullURL !== urlKey) {
+          window.location.replace(res.data.fullURL);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-  return (
-    <div>RedirectRoute</div>
-  )
-}
+  return <div>RedirectRoute</div>;
+};
 
-export default RedirectRoute
+export default RedirectRoute;
