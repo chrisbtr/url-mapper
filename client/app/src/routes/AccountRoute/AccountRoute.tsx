ef58ea793047db6f1375efb83c5ab26cd327798b
Chris <chrisbotros10@gmail.com>
2024-02-01 13:24:56 -0500

    feat: replace `CreateAccountForm` with `AccountLoginSwitcher` in the `AccountRoute` component

--- client/app/src/routes/AccountRoute/AccountRoute.tsx
+++ client/app/src/routes/AccountRoute/AccountRoute.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { TextFieldProps } from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import { isAxiosError } from "axios";
-import CreateAccountForm from "components/CreateAccountForm/CreateAccountForm";
+import AccountLoginSwitcher from "components/AccountLoginSwitcher/AccountLoginSwitcher";
 import SnackbarAlert from "components/SnackbarAlert/SnackbarAlert";
-import accountApi, { CreateAccountError } from "api/account";
+import accountApi, { CreateAccountError, AccountInfo } from "api/account";
 
 const AccountRoute: React.FC = () => {
+  const [accountInfo, setAccountInfo] = React.useState<AccountInfo | null>(
+    null
+  );
+
   const [usernameInput, setUsernameInput] = React.useState("");
   const [passwordInput, setPasswordInput] = React.useState("");
 
@@ -33,7 +37,6 @@ const AccountRoute: React.FC = () => {
 
   const handleCloseSnackbar = () => {
     setIsSnackbarOpen(false);
-    setAlertMessage("");
   };
 
   const handleUsernameInputChange: TextFieldProps["onChange"] = (event) => {
@@ -44,7 +47,26 @@ const AccountRoute: React.FC = () => {
     setPasswordInput(event.target.value);
   };
 
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+  const handleClearForm = () => {
+    setUsernameInput("");
+    setPasswordInput("");
+  };
+
+  const getAccountInfo = () => {
+    accountApi
+      .getAccount()
+      .then((res) => {
+        console.log(res.data)
+        setAccountInfo({...res.data});
+      })
+      .catch(() => {
+        console.log("Error fetching account data.")
+      });
+  };
+
+  const handleCreateAccountSubmit: React.FormEventHandler<HTMLFormElement> = (
+    event
+  ) => {
     event.preventDefault();
     console.log(event);
 
@@ -59,7 +81,7 @@ const AccountRoute: React.FC = () => {
         setUsernameErrorMessages(null);
         setPasswordErrorMessages(null);
 
-        openSnackbar("Successfully created mapping.", { isError: false });
+        openSnackbar("Successfully created account.", { isError: false });
       })
       .catch((err) => {
         if (isAxiosError<CreateAccountError>(err)) {
@@ -73,6 +95,45 @@ const AccountRoute: React.FC = () => {
       });
   };
 
+  const handleLoginSubmit: React.FormEventHandler<HTMLFormElement> = (
+    event
+  ) => {
+    event.preventDefault();
+
+    accountApi
+      .login({ username: usernameInput, password: passwordInput })
+      .then(() => {
+        setUsernameErrorMessages(null);
+        setPasswordErrorMessages(null);
+        getAccountInfo();
+
+        openSnackbar("Successfully logged in.", { isError: false });
+      })
+      .catch((err) => {
+        if (isAxiosError(err)) {
+          openSnackbar("Error: Invalid login info.", { isError: true });
+        } else {
+          openSnackbar("Unknown error.", { isError: true });
+        }
+      });
+  };
+
+  const handleLogout = () => {
+    setAccountInfo(null);
+    accountApi
+      .logout()
+      .then(() => {
+        openSnackbar("Successfully logged out.", { isError: false });
+      })
+      .catch(() => {
+        openSnackbar("Error: Could not logout.", { isError: true });
+      });
+  };
+
+  React.useEffect(() => {
+    getAccountInfo();
+  }, []);
+
   return (
     <Box
       sx={{
@@ -81,9 +142,12 @@ const AccountRoute: React.FC = () => {
         justifyContent: "center",
       }}
     >
-      <CreateAccountForm
-        paperFormProps={{ onSubmit: handleSubmit }}
-        usernameTextFieldProps={{
+      <AccountLoginSwitcher
+        onCreateAccountSubmit={handleCreateAccountSubmit}
+        onLoginSubmit={handleLoginSubmit}
+        onClearForm={handleClearForm}
+        createAccountFormProps={{
+          usernameTextFieldProps: {
           value: usernameInput,
           onChange: handleUsernameInputChange,
           error: usernameErrorMessages === null ? false : true,
@@ -94,8 +158,34 @@ const AccountRoute: React.FC = () => {
               ))}
             </>
           ),
+          },
+          passwordTextFieldProps: {
+            value: passwordInput,
+            onChange: handlePasswordInputChange,
+            error: passwordErrorMessages === null ? false : true,
+            helperText: (
+              <>
+                {passwordErrorMessages?.map((errorMessage) => (
+                  <>{errorMessage}</>
+                ))}
+              </>
+            ),
+          },
         }}
-        passwordTextFieldProps={{
+        loginFormProps={{
+          usernameTextFieldProps: {
+            value: usernameInput,
+            onChange: handleUsernameInputChange,
+            error: usernameErrorMessages === null ? false : true,
+            helperText: (
+              <>
+                {usernameErrorMessages?.map((errorMessage) => (
+                  <>{errorMessage}</>
+                ))}
+              </>
+            ),
+          },
+          passwordTextFieldProps: {
           value: passwordInput,
           onChange: handlePasswordInputChange,
           error: passwordErrorMessages === null ? false : true,
@@ -106,6 +196,7 @@ const AccountRoute: React.FC = () => {
               ))}
             </>
           ),
+          },
         }}
       />
       <SnackbarAlert
